474dc0b9b473b70f3af394b64a2e10af4c3cc2c5
Igor Mosunov <ileamo@yandex.ru>
2019-01-02 17:43:59 +0300

    Test output

--- lib/acari/iface.ex
+++ lib/acari/iface.ex
@@ -33,6 +33,9 @@ defmodule Acari.Iface do
     {:ok, ifsocket} = :tuncer.create(<<>>, [:tun, :no_pi, active: true])
     :tuncer.persist(ifsocket, false)
     ifname = :tuncer.devname(ifsocket)
+    Logger.info("#{tun_name}: iface #{ifname}: up")
+    :ok = if_up(ifname)
+    Logger.info("#{tun_name}: iface #{ifname}: up end")
     {:ok, ifsnd_pid} = Acari.IfaceSnd.start_link(%{ifsocket: ifsocket})
 
     #       System.cmd(
