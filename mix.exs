1f1a05556878d9e9adb563b4835677c4adccb4c4
Igor Mosunov <imosunov@nsg.ru>
2019-11-21 12:14:55 +0300

    1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Acari.MixProject do
   def project do
     [
       app: :acari,
-      version: "0.1.1",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
